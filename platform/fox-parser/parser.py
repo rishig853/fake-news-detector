eea54e81fecf0e0402bad51a2208b87673feab5b
rishig853 <rishig853@gmail.com>
2020-10-21 11:09:35 -0400

    Add fox parser code
    code prints out body text of article

--- platform/fox-parser/parser.py
+++ platform/fox-parser/parser.py
@@ -1,1 +1,13 @@
-#Parser for Fox News
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.foxnews.com/politics/biden-lowering-mask-cough-in-his-hand"
+
+request = requests.get(url)
+coverpage = request.content
+soup1 = BeautifulSoup(coverpage, 'html.parser')
+coverpage_news = soup1.find_all('p')
+text = ""
+for p in coverpage_news:
+    text += p.get_text()
+print(text)
