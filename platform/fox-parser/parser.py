f17492b692884fd3d6937bcd9ab96c90590adc5a
rishig853 <rishig853@gmail.com>
2020-10-23 13:39:11 -0400

    Update parser.py

--- platform/fox-parser/parser.py
+++ platform/fox-parser/parser.py
@@ -1,1 +1,15 @@
-#Parser for Fox News
+import requests
+from bs4 import BeautifulSoup
+#from flask import Flask
+#from flask_restful import Api, Resource
+
+url = "https://www.foxnews.com/politics/biden-lowering-mask-cough-in-his-hand"
+
+request = requests.get(url)
+coverpage = request.content
+soup1 = BeautifulSoup(coverpage, 'html.parser')
+coverpage_news = soup1.find_all('p')
+text = ""
+for p in coverpage_news:
+    text += p.get_text()
+print(text)
